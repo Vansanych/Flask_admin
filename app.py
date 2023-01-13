30f461eb8ff760535afd9bc3948aa2cd4ca2ed13
Иван <e741@mail.ru>
2023-01-13 12:17:20 +0300

    blueprint and db added

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask
 from config import Configuration
+from posts.blueprint import posts
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy as sa
+
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
+app.register_blueprint(posts, url_prefix='/blog')
+db = SQLAlchemy(app)
+
+sa.Column(sa.Integer)
+
