e436669348d1f4fc02c93796c9af18011e3195cb
Иван <e741@mail.ru>
2023-01-17 00:34:22 +0300

    refactored

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
 from config import Configuration
-from posts.blueprint import posts
 from flask_sqlalchemy import SQLAlchemy
-import sqlalchemy as sa
+
+from posts.blueprint import posts
 
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
-app.register_blueprint(posts, url_prefix='/blog')
+
 db = SQLAlchemy(app)
 
-sa.Column(sa.Integer)
+app.register_blueprint(posts, url_prefix='/blog')
+
 
