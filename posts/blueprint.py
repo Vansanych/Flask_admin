dc8309df1359f0498a5630afd676595ec5b0ec1a
Иван <e741@mail.ru>
2023-01-17 23:47:23 +0300

    posts added

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask import render_template
+from models import Post
 
 
 posts = Blueprint('posts', __name__, template_folder='templates')
@@ -7,4 +8,5 @@ posts = Blueprint('posts', __name__, template_folder='templates')
 
 @posts.route('/')
 def index():
-    return render_template('posts/index.html')
+    posts = Post.query.all()
+    return render_template('posts/index.html', posts=posts)
